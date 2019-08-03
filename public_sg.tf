acfab121cbc99eb07ff3c05f766964579923162d
jipara <zhypargul.esengulova@gmail.com>
2019-08-03 12:05:36 -0500

    ffff

--- public_sg.tf
+++ public_sg.tf
@@ -8,14 +8,15 @@ resource "aws_security_group" "public" {
     from_port   = 443
     to_port     = 443
     protocol    = "-1"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
 }
@@ -23,11 +24,11 @@ ingress {
   from_port   = 22
   to_port     = 22
   protocol    = "-1"
-  cidr_blocks = "[0.0.0.0/0]"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 ingress {
   from_port   = 80
   to_port     = 80
   protocol    = "-1"
-  cidr_blocks = "[0.0.0.0/0]"
+  cidr_blocks = ["0.0.0.0/0"]
 }
