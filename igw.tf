0a250d434753eda89f8de4b4894c12dbf02df5e6
jipara <zhypargul.esengulova@gmail.com>
2019-08-03 12:14:41 -0500

    aa

--- igw.tf
+++ igw.tf
@@ -1,6 +1,6 @@
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.dev.id}"
-
+}
   tags = {
     Name       = "${var.Name}"
     Env        = "${var.Env}"
