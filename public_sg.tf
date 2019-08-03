ee67435e215a7cde3f5e387737c36d5eac5ffcc4
jipara <zhypargul.esengulova@gmail.com>
2019-08-03 12:10:51 -0500

    aaa

--- public_sg.tf
+++ public_sg.tf
@@ -4,12 +4,23 @@ resource "aws_security_group" "public" {
   vpc_id      = "${aws_vpc.dev.id}"
 
   ingress {
-    # TLS (change to whatever ports you need)
     from_port   = 443
     to_port     = 443
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -19,19 +30,8 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
 
   }
-}
-ingress {
-  from_port   = 22
-  to_port     = 22
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-ingress {
-  from_port   = 80
-  to_port     = 80
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+
+
 
   tags = {
     Name       = "${var.Name}"
