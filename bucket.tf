b0f22c05a2182a718b3cd3141042d14ea8b72435
jipara <zhypargul.esengulova@gmail.com>
2019-08-04 14:24:25 -0500

    aaa

---
+++ bucket.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucketname" {
+  bucket = "insfrastructer-april-jipara-2"
+
+}
---
+++ dont_create.tf
@@ -0,0 +1,5 @@
+
+resource "aws_s3_bucket" "bucketnameb" {
+  bucket = "insfrastructer-april-jipara-2019b"
+
+}
