793e3e8374b0e0659d7070aac9b7f02ff4b4ad6a
jipara <zhypargul.esengulova@gmail.com>
2019-08-04 14:31:26 -0500

    comment

--- output.tf
+++ output.tf
@@ -4,9 +4,9 @@ output "VPC" {
 output "Public_subnet" {
   value = "${aws_subnet.public.id}"
 }
-output "Privet_subnet" {
-  value = "${aws_subnet.private.id}"
-}
+# output "Privet_subnet" {
+#   value = "${aws_subnet.private.id}"
+# }
 output "pub_sec_group" {
   value = "${aws_security_group.public.id}"
 }
