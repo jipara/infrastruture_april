9fc985c0305225f90a7c219d7b338e770e6b9040
jipara <zhypargul.esengulova@gmail.com>
2019-08-04 16:02:49 -0500

    capasite change

--- asg.tf
+++ asg.tf
@@ -19,51 +19,6 @@ module "wordpress" {
   health_check_type         = "EC2"
   min_size                  = 5
   max_size                  = 128
-  desired_capacity          = 4
+  desired_capacity          = 6
   wait_for_capacity_timeout = 0
-}
-module "asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "~> 3.0"
-
-  name = "service"
-
-  # Launch configuration
-  lc_name = "example-lc"
-
-  image_id        = "ami-ebd02392"
-  instance_type   = "t2.micro"
-  security_groups = ["sg-12345678"]
-
-  ebs_block_device = [
-    {
-      device_name           = "/dev/xvdz"
-      volume_type           = "gp2"
-      volume_size           = "50"
-      delete_on_termination = true
-    },
-  ]
-
-  root_block_device = [
-    {
-      volume_size = "50"
-      volume_type = "gp2"
-    },
-  ]
-
-  # Auto scaling group
-  asg_name                  = "example-asg"
-  vpc_zone_identifier       = ["subnet-1235678", "subnet-87654321"]
-  health_check_type         = "EC2"
-  min_size                  = 0
-  max_size                  = 1
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
-  tags = {
-    Name       = "${var.Name}"
-    Env        = "${var.Env}"
-    Created_by = "${var.Created_by}"
-    Dept       = "${var.Dept}"
-  }
-
 }
